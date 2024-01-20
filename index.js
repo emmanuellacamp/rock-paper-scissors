ffb39c8dd41e17113c9b1ca8ba1ffcc36491a2ee
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2024-01-20 19:58:14 +0100

    create computer choice function

---
+++ index.js
@@ -0,0 +1,9 @@
+function getComputerChoice () {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+
+
+
