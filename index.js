b246ae90ced13d15fe3bcb86f9ef5bb02cc7b0db
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2024-01-20 20:12:00 +0100

    create functin that plays a single round of rock paper scissors

--- index.js
+++ index.js
@@ -5,5 +5,23 @@ function getComputerChoice () {
 }
 
 
+function playRound(playerChoice, computerChoice) {
 
+    playerChoice = playerChoice.toLowerCase()
+    if (playerChoice === computerChoice) {
+        return 'Tie';
+    } else if (playerChoice === 'rock' && computerChoice === 'paper'){
+        return "You Lose! Paper beats Rock";
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+        return "You Lose! Rock beats Scissors";
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+        return "You Lose! Scissors beats Paper";
+    } else {
+        return "You Win!";
+    }
+};
+
+const playerChoice = 'paper';
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
 
