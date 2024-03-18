2e8950ee44cfaa54dc9a22e89ac80ae70a640eb3
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2024-03-18 01:01:50 +0100

    add style

--- index.js
+++ index.js
@@ -10,13 +10,29 @@ const scissorsBtn = document.getElementById('scissors');
 const restartBtn = document.getElementById('restart');
 const outcomeDiv = document.getElementById('outcome');
 const scoreDiv = document.getElementById('score');
+const userChoiceImg = document.getElementById('user-choice');
+const computerChoiceImg = document.getElementById('computer-choice');
 
 let playerScore = 0;
 let computerScore = 0;
 
+window.onload = function() {
+    // Hide score board initially
+    document.getElementById('result').style.display = 'none';
+};
+
 function playRound(playerChoice) {
     const computerChoice = getComputerChoice();
     let result;
+
+    // Update user choice icon
+    userChoiceImg.src = `images/${playerChoice}.png`;
+
+    // Update computer choice icon
+    computerChoiceImg.src = `images/${computerChoice}.png`;
+
+    document.getElementById('result').style.display = 'block';
+    document.getElementById('choose-weapon').style.display = 'none';
 
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice === computerChoice) {
@@ -31,9 +47,13 @@ function playRound(playerChoice) {
         computerScore++;
     }
     
-    outcomeDiv.textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
-    scoreDiv.textContent = `Score: ${playerScore} : ${computerScore}`;
+    // Update outcome text
+    outcomeDiv.textContent = `${result}`;
 
+    // Update score text
+    scoreDiv.textContent = `${computerScore} : ${playerScore}`;
+
+    // Check for game end
     if (playerScore === 5 || computerScore === 5) {
         if (playerScore > computerScore) {
             outcomeDiv.textContent += ` You won the game!`;
@@ -45,11 +65,17 @@ function playRound(playerChoice) {
     }
 }
 
+
+
 function restartGame() {
     playerScore = 0;
     computerScore = 0;
     outcomeDiv.textContent = '';
     scoreDiv.textContent = `Score: ${playerScore} : ${computerScore}`;
+    userChoiceImg.src = `images/paper.png`;
+    computerChoiceImg.src = `images/paper.png`;
+    document.getElementById('result').style.display = 'none';
+    document.getElementById('choose-weapon').style.display = 'block';
     enableButtons();
 }
 
@@ -65,6 +91,7 @@ function enableButtons() {
     scissorsBtn.disabled = false;
 }
 
+// Event listeners for buttons
 rockBtn.addEventListener('click', () => playRound('rock'));
 paperBtn.addEventListener('click', () => playRound('paper'));
 scissorsBtn.addEventListener('click', () => playRound('scissors'));
